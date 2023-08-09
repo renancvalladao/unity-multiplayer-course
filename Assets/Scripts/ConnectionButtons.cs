7334d022d65e3afc3e93128ee1e4e085c80ffd4e
Renan Valladão <renancvalladao@gmail.com>
2023-08-09 16:12:56 -0300

    Adding basic network authority

---
+++ Assets/Scripts/ConnectionButtons.cs
@@ -0,0 +1,15 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class ConnectionButtons : MonoBehaviour
+{
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+
+    public void StartClient()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+}
